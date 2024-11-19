ce7094549386d34d9494a73eab83504349e03cda
Shum-Steven <i.am.lucky.s.13@gmail.com>
2024-11-20 01:39:24 +0300

    Created README.md file with project description

--- obj/Debug/net9.0/ИКР.AssemblyInfo.cs
+++ obj/Debug/net9.0/ИКР.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ИКР")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f06e299cc6b801b8359431fff715d00e0ffb366")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b0f79dd8fbfcdf80b10e8583970faf85dbdc392")]
 [assembly: System.Reflection.AssemblyProductAttribute("ИКР")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ИКР")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
